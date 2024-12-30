34b681e2411a269b792a10cf4d14909587707c8a
shirshdixit <shirshdixitsdg35@gmail.com>
2024-12-31 01:47:28 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/learning/ui/theme/Dice.kt
@@ -0,0 +1,44 @@
+package com.example.learning.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onTertiary = Color.Black,
+    */
+)
+
+@Composable
+fun DiceRollerTheme(
+    darkTheme: Boolean = false, // Define if you want a dark theme or not
+    content: @Composable () -> Unit
+) {
+    val colors = if (darkTheme) {
+        DarkColorScheme
+    } else {
+        LightColorScheme
+    }
+
+    MaterialTheme(
+        colorScheme = colors,
+        typography = Typography,
+        content = content
+    )
+}
